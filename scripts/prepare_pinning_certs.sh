c5bad63794884916f1a99460e0415b2ea4003996
Taylor Weil <taylor.weil@workiva.com>
2023-07-26 14:15:46 -0500

    add script

--- scripts/prepare_pinning_certs.sh
+++ scripts/prepare_pinning_certs.sh
@@ -8,4 +8,4 @@ openssl s_client \
 openssl s_client \
   -servername httpbun.com \
   -connect httpbun.com:443 < /dev/null 2>/dev/null \
-  | openssl x509 -noout -fingerprint -sha256 > plugins/http2_adapter/test/_pinning_http2.txt 2>/dev/null
+  | openssl x509 -noout -fingerprint -sha256 > scripts/_pinning_http2.txt 2>/dev/null
